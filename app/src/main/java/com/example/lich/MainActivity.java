742fb3386ff46bbbb821ffb12831f9c3b9aef688
Thắng Đặng <dangthangtn123@gmail.com>
2022-03-02 09:30:21 +0700

    thang dang

--- app/src/main/java/com/example/lich/MainActivity.java
+++ app/src/main/java/com/example/lich/MainActivity.java
@@ -6,9 +6,11 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    CustomCalendar ct;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ct = (CustomCalendar) findViewById(R.id.custom);
     }
 }
